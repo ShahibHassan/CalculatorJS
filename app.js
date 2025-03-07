a50d367f43bd323e8f9878d7f43028c8148655c6
ShahibHassan <67552203+ShahibHassan@users.noreply.github.com>
2025-03-07 22:25:57 +0000

    created files for calculator project as well as basic operating functions

---
+++ app.js
@@ -0,0 +1,41 @@
+let num1;
+let operator;
+let num2;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(add(10, 2));
+console.log(subtract(10, 2));
+console.log(multiply(10, 2));
+console.log(divide(10, 2));
+
+function operate(operator) {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+      break;
+    case '-':
+      return num1 - num2;
+      break;
+    case '*':
+      return num1 * num2;
+      break;
+    case '/':
+      return num1 / num2;
+      break;
+  }
+}
